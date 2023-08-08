e26622739070c9ba64f25d54ca67453f6dff4532
minacky <nnabuifecyril@yahoo.com>
2023-08-08 09:25:08 +0100

    Rest-Api

--- littlelemon/restaurant/__init__.py
+++ littlelemon/restaurant/__init__.py
@@ -1,12 +1,12 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import MenuItemViewSet, CategoryViewSet, OrderViewSet
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from .views import MenuItemViewSet, CategoryViewSet, OrderViewSet
 
-router = DefaultRouter()
-router.register(r'menu-items', MenuItemViewSet)
-router.register(r'categories', CategoryViewSet)
-router.register(r'orders', OrderViewSet)
+# router = DefaultRouter()
+# router.register(r'menu-items', MenuItemViewSet)
+# router.register(r'categories', CategoryViewSet)
+# router.register(r'orders', OrderViewSet)
 
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
+# urlpatterns = [
+#     path('api/', include(router.urls)),
+# ]
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,16 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MenuItemViewSet, CategoryViewSet, OrderViewSet, MyObtainTokenPairView, RegisterView
+from .views import MenuItemViewSet, CategoryViewSet, OrderViewSet, MyObtainTokenPairView, RegisterView,ReservationViewSet
 from rest_framework_simplejwt.views import TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'menu-items', MenuItemViewSet)
 router.register(r'categories', CategoryViewSet)
 router.register(r'orders', OrderViewSet)
+router.register(r'reservation', ReservationViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+   
 ]
